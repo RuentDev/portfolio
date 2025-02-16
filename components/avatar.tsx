8ba2b91b20590c468dbad042a51219aa81fb30b1
RuentDev <ruent.dev@gmail.com>
2025-02-17 07:46:05 +0800

    update avatar hover

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -2,14 +2,21 @@ import Image from 'next/image'
 import React from 'react'
 const Avatar = () => {
   return (
-    <div className='w-[200px] h-[200px]'>
-			<Image 
-				src="/images/svg/mf-avatar.svg" 
-				alt="avatar" 
-				className="avatar-image object-cover object-center w-full"
-				width={200}
-				height={200}
-			/>
+    <div className='w-[200px] h-[200px] relative overflow-hidden rounded-full group'>
+		<Image 
+			src="/me.jpg" 
+			alt="avatar" 
+			width={200}
+			height={200}
+			className="avatar-image object-cover object-center w-full absolute top-0 left-0 opacity-0 scale-90 group-hover:scale-100 group-hover:opacity-100 transition-all ease-in-out duration-500"
+		/>
+		<Image 
+			src="/images/svg/mf-avatar.svg" 
+			alt="avatar" 
+			width={200}
+			height={200}
+			className="avatar-image object-cover object-center w-full absolute top-0 left-0 z-1 group-hover:scale-90 group-hover:opacity-0 transition-all ease-in-out duration-500"
+		/>
     </div>
   )
 }
